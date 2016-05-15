1a31f1775f7dd7d5d91d7bc430a482db804017ff
bob.sun <widekuan@outlook.com>
2016-05-15 21:09:52 +0800

    Wire up input method dialog.

--- app/src/main/java/sun/bob/leela/adapters/SimpleListAdapter.java
+++ app/src/main/java/sun/bob/leela/adapters/SimpleListAdapter.java
@@ -1,5 +1,7 @@
 package sun.bob.leela.adapters;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,17 +14,30 @@ import sun.bob.leela.db.Account;
 import sun.bob.leela.db.AccountHelper;
 import sun.bob.leela.db.Category;
 import sun.bob.leela.db.CategoryHelper;
+import sun.bob.leela.runnable.CryptoRunnable;
+import sun.bob.leela.services.IMEService;
+import sun.bob.leela.utils.CryptoUtil;
 
 /**
  * Created by bob.sun on 16/5/11.
  */
-public class SimpleListAdapter extends RecyclerView.Adapter<SimpleListViewHolder> {
+public class SimpleListAdapter extends RecyclerView.Adapter<SimpleListViewHolder> implements SimpleListViewHolder.SimpleListDelegate {
 
     private ArrayList data;
+    private Context context;
     @Override
     public SimpleListViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (context == null) {
+            context = parent.getContext();
+        }
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.simple_list_item, parent, false);
-        SimpleListViewHolder ret = new SimpleListViewHolder(view);
+        final SimpleListViewHolder ret = new SimpleListViewHolder(view);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ret.onClick();
+            }
+        });
         return ret;
     }
 
@@ -30,9 +45,11 @@ public class SimpleListAdapter extends RecyclerView.Adapter<SimpleListViewHolder
     public void onBindViewHolder(SimpleListViewHolder holder, int position) {
         Object item = data.get(position);
         if (item instanceof Account) {
-            holder.configureWithAccount((Account) item);
+            holder.configureWithAccount((Account) item, position);
+            holder.delegate = this;
         } else {
-            holder.configureWithCategory((Category) item);
+            holder.configureWithCategory((Category) item, position);
+            holder.delegate = this;
         }
     }
 
@@ -49,5 +66,35 @@ public class SimpleListAdapter extends RecyclerView.Adapter<SimpleListViewHolder
     public void loadAccountInCategory(Long category) {
         data = AccountHelper.getInstance(null).getAccountsByCategory(category);
         this.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onClick(SimpleListViewHolder.SimpleListViewType type, int index) {
+        switch (type) {
+            case SimpleListViewTypeAccount:
+                Account account = (Account) data.get(index);
+                new CryptoUtil(context, new CryptoUtil.OnDecryptedListener() {
+                    @Override
+                    public void onDecrypted(String account, String passwd, String addt) {
+                        Intent intent = new Intent(context, IMEService.class);
+                        intent.setAction("INIT");
+                        intent.putExtra("account", account);
+                        intent.putExtra("password", passwd);
+                        intent.putExtra("additional", addt);
+                        context.startService(intent);
+                        
+                        // TODO: 16/5/15 Publish an event to tell activity I'm done here.
+                        //               Dismiss that activity.
+
+                    }
+                }).runDecrypt(account.getAccount(), account.getHash(), account.getAdditional(),
+                        account.getAccount_salt(), account.getSalt(), account.getAdditional_salt());
+                break;
+            case SimpleListViewTypeCategory:
+                this.loadAccountInCategory(((Category) data.get(index)).getId());
+                break;
+            default:
+                break;
+        }
     }
 }
--- app/src/main/java/sun/bob/leela/adapters/SimpleListViewHolder.java
+++ app/src/main/java/sun/bob/leela/adapters/SimpleListViewHolder.java
@@ -20,23 +20,35 @@ public class SimpleListViewHolder extends RecyclerView.ViewHolder {
 
     private SimpleListViewType type;
     private View itemView;
+    private int index;
+    public SimpleListDelegate delegate;
 
     public SimpleListViewHolder(View itemView) {
         super(itemView);
         this.itemView = itemView;
     }
 
-    public void configureWithAccount(Account account) {
+    public void configureWithAccount(Account account, int index) {
         this.type = SimpleListViewType.SimpleListViewTypeAccount;
         ((AppCompatTextView) itemView.findViewById(R.id.account_name)).setText(account.getName());
+        this.index = index;
     }
 
-    public void configureWithCategory(Category category) {
+    public void configureWithCategory(Category category, int index) {
         this.type = SimpleListViewType.SimpleListViewTypeCategory;
         ((AppCompatTextView) itemView.findViewById(R.id.account_name)).setText(category.getName());
+        this.index = index;
     }
 
     public SimpleListViewType getType() {
         return type;
+    }
+
+    public void onClick() {
+        this.delegate.onClick(type, index);
+    }
+
+    interface SimpleListDelegate {
+        void onClick(SimpleListViewType type, int index);
     }
 }
