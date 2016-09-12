12101b3079fa96bdabfaaa82b269528e5dc76ef4
bob.sun <widekuan@outlook.com>
2016-09-12 22:40:39 +0800

    Add icons8 credit

--- app/src/main/java/sun/bob/leela/ui/activities/AboutActivity.java
+++ app/src/main/java/sun/bob/leela/ui/activities/AboutActivity.java
@@ -1,5 +1,7 @@
 package sun.bob.leela.ui.activities;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,19 +16,27 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        Element i8Element = new Element("icons8", "Icons from icons8.com",mehdi.sakout.aboutpage.R.drawable.about_icon_link);
+        i8Element.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse("https://icons8.com"));
+                startActivity(i);
+            }
+        });
+
         View aboutPage = new AboutPage(this)
                 .isRTL(false)
                 .setImage(R.drawable.ic_puff)
                 .setDescription(getString(R.string.about_puff))
-//                .addGroup("About Bcrypt")
-//                .addItem(getBcryptElement())
-//                .addWebsite("http://bcrypt.sourceforge.net/")
                 .addGroup("Connect with us")
                 .addEmail("bob.sun@outlook.ie")
                 .addWebsite("http://bobsun.website/")
                 .addGitHub("SpongeBobSun")
+                .addItem(i8Element)
                 .addGroup("About the name")
-                .addItem(new Element().setTitle("Blow fish Puffs!"))
+                .addGroup("Blowfish Puffs!")
                 .create();
 
         setContentView(aboutPage);
