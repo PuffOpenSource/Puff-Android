6d9c6acc520fe2de5bde6e75bbcf3107de426d70
bob.sun <widekuan@outlook.com>
2016-09-30 11:15:01 +0800

    Check in DAO Generator only

--- app/src/main/java/sun/bob/leela/events/DialogEvent.java
+++
@@ -1,7 +0,0 @@
-package sun.bob.leela.events;
-
-/**
- * Created by bob.sun on 16/5/21.
- */
-public class DialogEvent {
-}
--- material-intro/src/main/java/com/heinrichreimersoftware/materialintro/slide/RestorableSlide.java
+++
@@ -1,7 +0,0 @@
-package com.heinrichreimersoftware.materialintro.slide;
-
-import android.support.v4.app.Fragment;
-
-public abstract class RestorableSlide extends Slide {
-    public abstract void setFragment(Fragment fragment);
-}
